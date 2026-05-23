611151e7d5de2842e554a1620ade382e6c113d50
dgSloxx417 <donkorgerald0@gmail.com>
2026-05-23 09:00:27 +0000

    03: Implement PostHog

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import Image from 'next/image'
 import Link from 'next/link'
+import posthog from 'posthog-js'
 
 
 const Navbar = () => {
+  const captureNavClick = (label: string) => {
+    posthog.capture("navbar_link_clicked", { label });
+  };
+
   return (
     <header>
       <nav>
-        <Link href="/" className="logo">
+        <Link href="/" className="logo" onClick={() => captureNavClick("logo")}>
           <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
 
           <p>ShowEvents</p>
         </Link>
 
         <ul>
-          <Link href="/">Home</Link>
-          <Link href="/">Events</Link>
-          <Link href="/">Create Event</Link>
+          <Link href="/" onClick={() => captureNavClick("Home")}>Home</Link>
+          <Link href="/" onClick={() => captureNavClick("Events")}>Events</Link>
+          <Link href="/" onClick={() => captureNavClick("Create Event")}>Create Event</Link>
         </ul>
       </nav>
     </header>
